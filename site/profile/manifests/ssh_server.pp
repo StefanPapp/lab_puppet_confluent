74c7d7ecd2502db7454490cd4632ae6551c2d2f0
Stefan Papp <stefan.papp@data-wizard.net>
2019-11-29 17:53:41 +0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDRvoIVFpf2siWdvQELm83ANg+j1tL9sjmUNc/DUJj3hV8ZLMJOx9oSwlNIuE+RzwWos9WNp5kfZYyHCpmaAf6TTpngFwq3SYIZ0rX9/TshzITz4zCUCzX07UcF+HvTXbv57+VnFrmIttH3B9vf20a4Rr3p+PoiuQKLwkbpC9ti12JS71hZ+hprlvKcMEtDR1YXd8P5TDUc90BtOgsIclvaipoV1vlsic3GIGArubsAsdhcjh8IdJEF//368isGUMyRJjyRC1OI7QTTRrED8xZ9qAe3Naz+nRDfUPpAKwOlKfVFBo5RkWnlhJ+9qGJMLHCDIB3SdAebUzpFXjolA3rz',
 	}  
 }
